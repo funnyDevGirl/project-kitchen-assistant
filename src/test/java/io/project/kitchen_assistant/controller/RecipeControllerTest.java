7285fe26245543066158fa3901bef9f06fab2ef4
alina tarasova <alishka.tarasova@gmail.com>
2024-12-11 11:10:49 +0300

    Fix project #2

--- src/test/java/io/project/kitchen_assistant/controller/RecipeControllerTest.java
+++ src/test/java/io/project/kitchen_assistant/controller/RecipeControllerTest.java
@@ -18,15 +18,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-
 import static java.lang.String.format;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,6 +41,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Testcontainers
+@ActiveProfiles("test")
 public class RecipeControllerTest {
     @Autowired
     private MockMvc mockMvc;
